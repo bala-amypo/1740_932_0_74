fafb54a88656f1c3b56e1ecc05134ef24c632fce
bala-amypo <bala@amypo.in>
2025-12-19 05:27:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -7,7 +7,8 @@ import lombok.Data;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Max;
+
+import jakarta.validation.constraints.Size;
 
 @Data
 @Entity
